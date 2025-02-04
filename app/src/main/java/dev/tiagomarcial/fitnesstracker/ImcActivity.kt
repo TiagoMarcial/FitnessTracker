4972e228cf45b3e2f68f1c7d5e74aa08d1cda626
Tiago Marcial <tiko127@hotmail.com>
2025-02-04 02:52:06 -0300

    Desenvolvendo a interface para calculo de IMC

--- app/src/main/java/dev/tiagomarcial/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/dev/tiagomarcial/fitnesstracker/ImcActivity.kt
@@ -10,5 +10,6 @@ class ImcActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+        
     }
 }
