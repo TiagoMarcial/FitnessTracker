c35490a4503855758f1980174bad6e5c65ecaa9c
Tiago Marcial <tiko127@hotmail.com>
2025-02-04 03:23:01 -0300

    Desenvolvendo a interface para calculo de IMC

--- app/src/main/java/dev/tiagomarcial/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/dev/tiagomarcial/fitnesstracker/ImcActivity.kt
@@ -1,15 +1,44 @@
 package dev.tiagomarcial.fitnesstracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
-        
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if (!validate()){
+                Toast.makeText(this,R.string.field_messages, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+        }
+    }
+
+    private fun validate(): Boolean {
+        if (editHeight.text.toString().isNotEmpty()
+            && editWeight.text.toString().isNotEmpty()
+            && !editWeight.text.toString().startsWith("0")
+            && !editHeight.text.toString().startsWith("0")) {
+        return true
+        } else {
+            return false
+        }
     }
 }
